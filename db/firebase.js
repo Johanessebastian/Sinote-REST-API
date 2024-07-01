06ae44689d5fea6153ab7504765933dfa623a62d
johanessebastian <anesdiman@gmail.com>
2024-07-01 09:39:52 +0700

    update secret

--- db/firebase.js
+++ db/firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./sinote-c7912-firebase-adminsdk-82ex8-9414a1de51.json'); // Adjust the path
+const serviceAccount = require(''); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://sinote-c7912-default-rtdb.asia-southeast1.firebasedatabase.app/', // Replace with your DB URL
+  databaseURL: '', 
 });
 
 const db = admin.firestore();
